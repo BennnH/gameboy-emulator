306dd9be95e940fc4ee83e6df8a00e9f20316d13
BennnH <ben.hardy.uk@gmail.com>
2026-07-31 14:30:50 +0100

    Refactored Bus into discrete memory regions with address routing

--- src/bus.cpp
+++ src/bus.cpp
@@ -1,11 +1,34 @@
 #include "bus.h"
 #include "cartridges/cartridge.h"
 
-#include <cstdio>
+#include <stdio.h>
 
 
 Bus::Bus(Cartridge& cartridge) : cartridge_(cartridge) {
 }
+
+uint8_t Bus::read_io(uint16_t address) const {
+    // Interupt flag register. Top 3 bits aren't used and always have value '1'
+    if (address == 0xFF0F) {
+        return io_[address - 0xFF00] | 0xE0;
+    }
+    return io_[address - 0xFF00];
+}
+
+
+void Bus::write_io(uint16_t address, uint8_t value) {
+    // Serial data transfer, outputs the byte when a transfer is requested so we can debug through terminal output.
+    if (address == 0xFF02 && value == 0x81) {
+        std::printf("%c", io_[0xFF01 - 0xFF00]);
+        // Force output the contents, wouldn't happen without this in an inf loop
+        std::fflush(stdout);
+        // Clear to signal the transfer is done which is done by actual hardware.
+        io_[0xFF02 - 0xFF00] = 0x01;
+        return;
+    }
+    io_[address - 0xFF00] = value;
+}
+
 
 uint8_t Bus::read8(uint16_t address) const {
     // Memory region for rom/cartridge (0x0000 - 0x7FFF)
@@ -13,28 +36,89 @@ uint8_t Bus::read8(uint16_t address) const {
         return cartridge_.read(address);
     }
 
-    if (address == 0xFF0F) {
-        return this->memory_[address] | 0xE0;
+    if ((address >= 0x8000) && (address <= 0x9FFF)) {
+        return vram_[address - 0x8000];
     }
-    // For now everything else is just this temp memory array for now
-    return this->memory_[address];
+
+    if ((address >= 0xA000) && (address <= 0xBFFF)) {
+        // This is for MBC later so just returning a placeholder value for now.
+        return 0xFF;
+    }
+
+    if ((address >= 0xC000) && (address <= 0xDFFF)) {
+        return wram_[address - 0xC000];
+    }
+
+    if ((address >= 0xE000) && (address <= 0xFDFF)) {
+        // This is a mirror of WRAM
+        return wram_[address - 0xE000];
+    }
+
+    if ((address >= 0xFE00) && (address <= 0xFE9F)) {
+        return oam_[address - 0xFE00];
+    }
+
+    if ((address >= 0xFEA0) && (address <= 0xFEFF)) {
+        // Nintendo says use of this area is prohibited
+        return 0xFF;
+    }
+
+    if ((address >= 0xFF00) && (address <= 0xFF7F)) {
+        return read_io(address);
+    }
+
+    if ((address >= 0xFF80) && (address <= 0xFFFE)) {
+        return hram_[address - 0xFF80];
+    }
+
+    return ie_;
 }
+
 
 void Bus::write8(uint16_t address, uint8_t value) {
     // For now roms are read only, and we don't write to cartridges so ignore.
     if (address <= 0x7FFF) {
         return;
     }
-    this->memory_[address] = value;
 
-    if (address == 0xFF02 && value == 0x81) {
-        std::printf("%c", this->memory_[0xFF01]);
-        // Force output the contents, wouldn't happen without this in an inf loop
-        std::fflush(stdout);
-        // Clear to signal the transfer is done which is done by actual hardware.
-        this->memory_[0xFF02] = 0x01;
+    if ((address >= 0x8000) && (address <= 0x9FFF)) {
+        vram_[address - 0x8000] = value;
+    }
+
+    if ((address >= 0xA000) && (address <= 0xBFFF)) {
+        // Used for MBC, ignore until later.
+    }
+
+    if ((address >= 0xC000) && (address <= 0xDFFF)) {
+        wram_[address - 0xC000] = value;
+    }
+
+    if ((address >= 0xE000) && (address <= 0xFDFF)) {
+        // This is a mirror of WRAM
+        wram_[address - 0xE000] = value;
+    }
+
+    if ((address >= 0xFE00) && (address <= 0xFE9F)) {
+        oam_[address - 0xFE00] = value;
+    }
+
+    if ((address >= 0xFEA0) && (address <= 0xFEFF)) {
+        // Nintendo says use of this area is prohibited
+    }
+
+    if ((address >= 0xFF00) && (address <= 0xFF7F)) {
+        write_io(address, value);
+    }
+
+    if ((address >= 0xFF80) && (address <= 0xFFFE)) {
+        hram_[address - 0xFF80] = value;
+    }
+
+    if (address == 0xFFFF) {
+        ie_ = value;
     }
 }
+
 
 void Bus::tick(int cycles) {
     cycles_ += cycles;
@@ -44,22 +128,22 @@ void Bus::tick(int cycles) {
     // Increment the DIV register if it overflows
     while (div_counter_ >= 256) {
         div_counter_ -= 256;
-        memory_[0xFF04]++;
+        io_[0xFF04 - 0xFF00]++;
     }
 
     // TIMA, only if the enbled bit is on in the TAC register
-    if (memory_[0xFF07] & 0x04){
+    if (io_[0xFF07 - 0xFF00] & 0x04){
         tima_counter_ += cycles;
         int tima_threshold = get_tac_speed();
 
         while (tima_counter_ >= tima_threshold) {
             tima_counter_ -= tima_threshold;
-            if (memory_[0xFF05] == 0xFF) {
+            if (io_[0xFF05 - 0xFF00] == 0xFF) {
                 // This increment would make it overflow, so we reset to TMA (the reset value) and raise the timer interupt.
-                memory_[0xFF05] = memory_[0xFF06]; // TMA
-                memory_[0xFF0F] |= 0x04; // raise the timer bit of interupt flag register.
+                io_[0xFF05 - 0xFF00] = io_[0xFF06 - 0xFF00]; // TMA
+                io_[0xFF0F - 0xFF00] |= 0x04; // raise the timer bit of interupt flag register.
             } else {
-                memory_[0xFF05]++;
+                io_[0xFF05 - 0xFF00]++;
             }
         }
     }
@@ -67,7 +151,7 @@ void Bus::tick(int cycles) {
 
 
 int Bus::get_tac_speed() const{
-    switch (memory_[0xFF07] & 0x03) {
+    switch (io_[0xFF07 - 0xFF00] & 0x03) {
         case 0x00:
             return 1024;
         case 0x01:
--- src/bus.h
+++ src/bus.h
@@ -12,13 +12,28 @@ class Bus {
         uint8_t read8(uint16_t address) const;
         void write8(uint16_t address, uint8_t value);
 
+        uint8_t read_io(uint16_t address) const;
+        void write_io(uint16_t address, uint8_t value);
+
         void tick(int cycles);
 
         int get_tac_speed() const;
     private:
         Cartridge& cartridge_;
-        // Used for now for non rom regions.
-        std::array<uint8_t, 0x10000> memory_{};
+        // Memory regions from pandocs memory map.
+
+        // 0x8000-0x9FFF
+        std::array<uint8_t, 0x2000> vram_{};
+        // 0xC000-0xDFFF
+        std::array<uint8_t, 0x2000> wram_{};
+        // 0xFE00-0xFE9F
+        std::array<uint8_t, 0xA0> oam_{};
+        // 0xFF00-0xFF7F
+        std::array<uint8_t, 0x80> io_{};
+        // 0xFF80-0xFFFE
+        std::array<uint8_t, 0x7F> hram_{};
+        // 0xFFFF - Interrupt Enable register
+        uint8_t ie_{};
 
         // Total t cycles since boot
         uint64_t cycles_{};
